a0c9e7bbcb654b251ba6750489fbec5e6b247b7c
Alex <urbinagonzalezja@gmail.com>
2021-04-07 13:22:35 +0200

    Import the form with one tag div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulary from './components/Formulary';
 
 
 function App() {
@@ -8,6 +9,19 @@ function App() {
       <Header 
         title='Weather React App'
       />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulary />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
