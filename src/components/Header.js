fab7e83e75e55491901697a1ef6034c206777719
Alex <urbinagonzalezja@gmail.com>
2021-04-07 14:33:29 +0200

    We only change the color tone

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({title}) => {
     return (
         <nav>
-            <div className="nav-wrapper light-blue darken-9">
+            <div className="nav-wrapper light-blue darken-4">
                 <a href="#!" className="brand-logo">{title}</a>
             </div>
         </nav>
