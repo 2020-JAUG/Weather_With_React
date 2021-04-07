7200d1cb475b4e4cc8274b42e83cfc7e55bb5f8c
Alex <urbinagonzalezja@gmail.com>
2021-04-07 14:08:40 +0200

    We add the event to change the state

--- src/components/Formulary.js
+++ src/components/Formulary.js
@@ -2,10 +2,21 @@ import React, { useState } from 'react'
 
 const Formulary = () => {
 
-    const [lokkFor, setLookFor] = useState ({
+    const [lookFor, setLookFor] = useState ({
         ciudad: '',
         pais: ''
     });
+
+    //Destructuring
+    const {ciudad, pais} = lookFor;
+
+    //To put the elements in the state
+    const handleChange = e => {
+        setLookFor({
+            ...lookFor,
+            [e.target.name] : e.target.value
+        });
+    }
 
     return ( 
         <form>
@@ -14,21 +25,29 @@ const Formulary = () => {
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="ciudad">City: </label>
             </div>
 
             <div className="input-field col s12">
-                <select name="pais">
-                    <option value="">-- Select a Country --</option>
-                        <option value="VE">Venezuela</option>
-                        <option value="PH">Philippines</option>
-                        <option value="US">United States of America</option>
-                        <option value="ES">Spain</option>
-                        <option value="MX">México</option>
-                        <option value="AR">Argentina</option>
-                        <option value="CR">Costa Rica</option>
-                        <option value="JP">Japan</option>
+                <select 
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                    >
+                        
+                        <option value="">-- Select a Country --</option>
+                            <option value="VE">Venezuela</option>
+                            <option value="PH">Philippines</option>
+                            <option value="US">United States of America</option>
+                            <option value="ES">Spain</option>
+                            <option value="MX">México</option>
+                            <option value="AR">Argentina</option>
+                            <option value="CR">Costa Rica</option>
+                            <option value="JP">Japan</option>
                 </select>
                 <label htmlFor="pais">Country: </label>
             </div>
