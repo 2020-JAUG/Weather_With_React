260e896287047b188420a66a6966e9d63e949f13
Alejandro <urbinagonzalezja@gmail.com>
2021-04-08 19:12:51 +0200

    We pass message error and import  Errorcomponent

--- src/components/Formulary.js
+++ src/components/Formulary.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Error from './Error';
 
 const Formulary = ({lookFor, setLookFor, setConsult}) => {
 
@@ -33,7 +34,9 @@ const Formulary = ({lookFor, setLookFor, setConsult}) => {
         <form
             onSubmit={handleSubmit}
         >
-            { error ? <p className="red darken-4 error">All fields are required</p> : null }
+
+        { error ? <Error message='Both fields are required' /> : null }
+
             <div className="input-field col s12">
                 <input
                     type="text"
