a03d3934177309c65ac0c578e4afb56662f41b07
Alejandro <urbinagonzalezja@gmail.com>
2021-04-09 10:29:00 +0200

    We add coord, Humidity, Pressure, Lat and Lon

--- src/components/Clime.js
+++ src/components/Clime.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Clime = ({result}) => {
 
     //Extract the values
-    const { name, main } = result;
+    const { name, main, coord } = result;
 
     if(!name) return null;
 
@@ -18,11 +18,23 @@ const Clime = ({result}) => {
                 <p className='temperatura'>
                     { parseFloat( main.temp - kelvin, 10 ).toFixed(2) } <span> &#x2103; </span>
                 </p>
-                <p>Maximum temperature:
+                 <p>Maximum temperature:
                     { parseFloat( main.temp_max - kelvin, 10 ).toFixed(2) } <span> &#x2103; </span>
                 </p>
                 <p>Minimum temperature:
                     { parseFloat( main.temp_min - kelvin, 10 ).toFixed(2) } <span> &#x2103; </span>
+                </p>
+                <p>Humidity:
+                    { ( main.humidity ) } <span> &#37; </span>
+                </p>
+                <p>Pressure:
+                    { ( main.pressure ) } <span> &#104;&#80;&#97; </span>
+                </p>
+                <p>Lat:
+                    { parseFloat( coord.lat ).toFixed(2) } <span> &#176;&#78; </span>
+                </p>
+                <p>Lon:
+                    { parseFloat( coord.lon ).toFixed(2) } <span> &#176;&#69; </span>
                 </p>
             </div>
         </div>
